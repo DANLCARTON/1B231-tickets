41d062af7159ba9a2f888fbfb2ec5493ee98ef37
DANLCARTON <ericthiberge@orange.fr>
2023-09-27 11:37:33 +0200

    setup

---
+++ app/tickets/TicketList.jsx
@@ -0,0 +1,31 @@
+import Link from "next/link"
+
+const getTickets = async () => {
+    await new Promise((resolve) => setTimeout(resolve, 1000))
+    const res = await fetch('http://localhost:4000/tickets', {
+        next: {
+            revalidate: 0
+        }
+    })
+    return res.json();
+}
+
+export default async function TicketList() {
+    const tickets = await getTickets();
+    return (
+        <>
+            {tickets.map((ticket) => (
+                <Link href={`./tickets/${ticket.id}`} key={ticket.id}>
+                    <div key={ticket.id} className="card my-5" > 
+                        <h3>{ticket.title}</h3>
+                        <p>{ticket.body.slice(0, 200)}...</p>
+                        <div className={`pill ${ticket.priority}`}>{ticket.priority} priority</div>
+                    </div>
+                </Link>
+            ))}
+            {tickets.length === 0 && (
+                <p className="text-center">No open tickets !</p>
+            )}
+        </>
+    )
+}
